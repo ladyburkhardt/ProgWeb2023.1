d54556aa3cf6a8b9b22f197d8d2383965c4bb2ef
ladyburkhardt <104326400+ladyburkhardt@users.noreply.github.com>
2023-03-31 19:32:26 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ProgWeb2023.1
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-icmz9c)
---
+++ index.js
@@ -0,0 +1,4 @@
+function rolarDado() {
+  var resultado = Math.floor(Math.random() * 6) + 1;
+  document.getElementById("resultado").innerHTML = resultado;
+  }
