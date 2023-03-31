e3d8adeb0d3af118c595d14d58fa38a8af773f1c
ladyburkhardt <104326400+ladyburkhardt@users.noreply.github.com>
2023-03-31 19:50:46 -0300

    2 files modified

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-function rolarDado() {
+const botao = document.getElementById("dado");
+
+function dadinho() {
   var resultado = Math.floor(Math.random() * 6) + 1;
-  document.getElementById("resultado").innerHTML = resultado;
+  document.getElementById("lado").innerHTML = resultado;
   }
+
+  botao.onclick=dadinho;
